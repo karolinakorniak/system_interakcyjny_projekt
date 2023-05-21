cce678151e2308943916a3371732818acae25c27
Karolina Korniak <karolinakorniak85@gmail.com>
2023-05-21 18:36:52 +0200

    used annotation to route questions controller

--- src/Controller/QuestionsController.php
+++ src/Controller/QuestionsController.php
@@ -3,11 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
 class QuestionsController
 {
+    #[Route('/questions')]
     public function index(): Response
     {
-
         return new Response(
             '<html><body>Tutaj beda pytania</body></html>'
         );
