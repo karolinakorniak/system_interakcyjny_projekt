0ce3ef41ff7cb4e763568bd75ee1721de0e73ea5
Karolina Korniak <karolinakorniak85@gmail.com>
2023-05-27 22:20:18 +0200

    Added route for single question

--- src/Controller/QuestionsController.php
+++ src/Controller/QuestionsController.php
@@ -6,13 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route("/questions")]
 class QuestionsController extends AbstractController
 {
-    #[Route('/questions')]
+    #[Route('/')]
     public function index(): Response
     {
         return $this->render(
             'questions/index.html.twig',
+        );
+    }
+
+    #[Route('/{slug}')]
+    public function singleQuestion(string $slug): Response
+    {
+        return $this->render(
+            'single.html.twig',
+            ['slug' => $slug]
         );
     }
 }
