441c91211f24324591e473078677bbef7eb11923
karolinakorniak <karolinakorniak85@gmail.com>
2023-06-06 20:43:28 +0200

    used UserDataRepository in AuthorsController

--- src/Controller/AuthorsController.php
+++ src/Controller/AuthorsController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Repository\AuthorsRepository;
+use App\Repository\UserDataRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class AuthorsController extends AbstractController
 {
     #[Route('/')]
-    public function index(AuthorsRepository $repository): Response
+    public function index(UserDataRepository $repository): Response
     {
         return $this->render(
             'authors/index.html.twig',
