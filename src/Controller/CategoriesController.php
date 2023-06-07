3f8387e1f5e75cd29e11088415eeea0ff0fce182
karolinakorniak <karolinakorniak85@gmail.com>
2023-06-07 19:33:23 +0200

    categories title

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,11 +13,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class CategoriesController extends AbstractController
 {
     #[Route('/', name: 'category_index')]
-    public function index(CategoryRepository $repository): Response
+    public function index(Request $request, CategoryRepository $repository, PaginatorInterface $paginator): Response
     {
+        $pagination = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            CategoryRepository::PAGINATOR_ITEMS_PER_PAGE,
+        );
+
         return $this->render(
             'categories/index.html.twig',
-            ['categories' => $repository->findAll()]
+            ['pagination' => $pagination]
         );
     }
 }
