3a208b46ba4400eba89e0b04c3b8436eade31820
karolinakorniak <karolinakorniak85@gmail.com>
2023-06-08 02:37:16 +0200

    real implementation for user

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,7 +5,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Enum\UserRole;
 use App\Entity\User;
+use App\Entity\UserData;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 /**
  * Class UserFixtures.
@@ -15,16 +18,40 @@ class UserFixtures extends AbstractBaseFixtures
     public static int $USER_COUNT = 2;
 
     /**
+     * Password hasher.
+     */
+    private UserPasswordHasherInterface $passwordHasher;
+
+    /**
+     * @param UserPasswordHasherInterface $passwordHasher Password hasher
+     */
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
+
+    /**
      * Load data.
      */
     public function loadData(): void
     {
-        $this->createMany(self::$USER_COUNT, "users", function () {
+        $this->createMany(self::$USER_COUNT, "users", function ($i) {
             $user = new User();
-            $user->setEmail($this->faker->email);
+            $user->setEmail(sprintf('admin%d@example.com', $i));
+            $user->setRoles([UserRole::ROLE_ADMIN->value]);
             $user->setPassword(
-                $this->faker->password
+                $this->passwordHasher->hashPassword(
+                    $user,
+                    'user1234'
+                )
             );
+
+            $userData = new UserData();
+            $userData->setName($this->faker->userName);
+            $userData->setDescription($this->faker->realTextBetween(110, 220));
+
+            $user->setUserData($userData);
 
             return $user;
         });
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -5,6 +5,9 @@ namespace App\Repository;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 
 /**
  * @extends ServiceEntityRepository<User>
@@ -14,10 +17,9 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class UserRepository extends ServiceEntityRepository
+class UserRepository extends ServiceEntityRepository implements PasswordUpgraderInterface
 {
     public const PAGINATOR_ITEMS_PER_PAGE = 10;
-
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
@@ -39,6 +41,20 @@ class UserRepository extends ServiceEntityRepository
         if ($flush) {
             $this->getEntityManager()->flush();
         }
+    }
+
+    /**
+     * Used to upgrade (rehash) the user's password automatically over time.
+     */
+    public function upgradePassword(PasswordAuthenticatedUserInterface $user, string $newHashedPassword): void
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', \get_class($user)));
+        }
+
+        $user->setPassword($newHashedPassword);
+
+        $this->save($user, true);
     }
 
 //    /**
